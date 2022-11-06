1f91715a1fdf5702ca73fe4137c17e32cebb01ef
tang <296429819@qq.com>
2022-11-07 00:55:00 +0800

    change to loda13

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: '1oda',
+  github: 'loda13',
   // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
