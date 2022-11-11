757a9afe6b3743110d530f22c227aa7f8d150341
tang <296429819@qq.com>
2022-11-11 23:30:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,12 +14,12 @@ export default siteConfig({
   author: 'loda Tang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Loda Tang‘s blog',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
-  // github: 'loda13',
-  // linkedin: 'fisch2',
+  twitter: 'transitive_bs',
+  github: 'loda13',
+  linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
