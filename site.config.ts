25c2df76083b09785e4d49f64d6dacbefc912405
tang <296429819@qq.com>
2022-11-12 21:08:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -49,24 +49,24 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  // navigationStyle: 'default'
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'About',
-      pageId: '597b1b1465574370bd8d5d32f7b9e65d'
-    },
-    {
-      title: 'Posts',
-      pageId: '23f02b6a347243ef9f269673a741db71'
-    },
-    {
-      title: 'Music',
-      pageId: 'b8473e26e93945c1bba36105c4f09747'
-    },
-    {
-      title: 'Contact',
-      pageId: '8252bc90ba484a4ea2c5123c0256d258'
-    }
-  ]
-})
+  navigationStyle: 'default'
+//   navigationStyle: 'custom',
+//   navigationLinks: [
+//     {
+//       title: 'About',
+//       pageId: '597b1b1465574370bd8d5d32f7b9e65d'
+//     },
+//     {
+//       title: 'Posts',
+//       pageId: '23f02b6a347243ef9f269673a741db71'
+//     },
+//     {
+//       title: 'Music',
+//       pageId: 'b8473e26e93945c1bba36105c4f09747'
+//     },
+//     {
+//       title: 'Contact',
+//       pageId: '8252bc90ba484a4ea2c5123c0256d258'
+//     }
+//   ]
+// })
