83918136f49c8a194f12f4837bfd63e29435f7fd
tang <296429819@qq.com>
2023-01-05 11:33:24 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright © 2023 {config.author}. All rights reserved.</div>
+      <div className={styles.copyright}>Copyright © new Date().getFullYear() {config.author}. All rights reserved.</div>
 
       <div className={styles.settings}>
         {hasMounted && (
